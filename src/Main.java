3c1712772cc9f2b8e652c92ad3de118531447ad8
Zankooo <stankovic.zan@gmail.com>
2025-07-29 23:08:08 +0200

    fixing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("burek");
+
+    private static ConcurrentHashMap<String, AtomicInteger> nGrams = new ConcurrentHashMap<>();
+
+    public static String izbiraTeksta() {
+        String[] datoteke = {"123MB.txt", "234MB.txt", "350MB.txt", "490MB.txt", "613MB.txt"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Izberi besedilo:");
+        for (int i = 0; i < datoteke.length; i++) {
+            System.out.println((i+1) + ". " + datoteke[i]);
+        }
+        while (true) {
+            System.out.print("Vaša izbira (1-" + datoteke.length + "): ");
+            if (scanner.hasNextInt()) {
+                int izbira = scanner.nextInt();
+                if (izbira >= 1 && izbira <= datoteke.length) {
+                    return "resources/" + datoteke[izbira-1];
+                }
+            } else {
+                scanner.next();
+            }
+            System.out.println("Neveljavna izbira. Prosimo, vnesite številko med 1 in " + datoteke.length + ".");
+        }
     }
-}
+
+    public static void main(String[] args) {
+    }
+    }
